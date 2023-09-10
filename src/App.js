0e3a31be8d94f877a8cbfbe77c67fa8c6fd9a227
songbbang <rokaf340@gmail.com>
2023-09-10 17:25:45 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
+import requests from './api/requests';
 import Banner from './component/Banner';
 import Nav from './component/Nav';
+import Row from './component/Row';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Nav />
       <Banner />
+      <Row
+        title='NETFLIX ORIGINALS'
+        id='NO'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      ></Row>
+      <Row
+        title='Action Movies'
+        id='AM'
+        fetchUrl={requests.fetchActionMovies}
+      ></Row>{' '}
+      <Row
+        title='Comedy Movies'
+        id='CM'
+        fetchUrl={requests.fetchComedyMovies}
+      ></Row>{' '}
+      <Row
+        title='Horror Movies'
+        id='HM'
+        fetchUrl={requests.fetchHorrorMovies}
+      ></Row>{' '}
+      <Row
+        title='Romance Movies'
+        id='RM'
+        fetchUrl={requests.fetchRomanceMovies}
+      ></Row>
     </div>
   );
 }
--- src/component/Banner.js
+++ src/component/Banner.js
@@ -2,11 +2,15 @@ import axios from '../api/axios';
 import React, { useEffect, useState } from 'react';
 import requests from '../api/requests';
 import './Banner.css';
+import { styled } from 'styled-components';
+
 const Banner = () => {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
+
+  const [isClicked, setIsClicked] = useState(false);
 
   const fetchData = async () => {
     // 영화 정보 가져오기
@@ -21,36 +25,83 @@ const Banner = () => {
     const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
       params: { append_to_response: 'videos' },
     });
-    console.log({ data: movieDetail });
+    // console.log({ data: movieDetail });
     setMovie(movieDetail);
   };
-  return (
-    <header
-      className='banner'
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-        backgroundPosition: 'top center',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div className='banner__contents'>
-        {/* title */}
-        <h1 className='banner__title'>
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
-        <div className='banner__buttons'>
-          <button className='banner__button play'>Play</button>
-          <button className='banner__button info'>
-            <div className='space'></div> More Infomation
-          </button>
+  if (!isClicked) {
+    return (
+      <header
+        className='banner'
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+          backgroundPosition: 'top center',
+          backgroundSize: 'cover',
+        }}
+      >
+        <div className='banner__contents'>
+          {/* title */}
+          <h1 className='banner__title'>
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className='banner__buttons'>
+            <button
+              className='banner__button play'
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
+            <button className='banner__button info'>
+              <div className='space'></div> More Infomation
+            </button>
+          </div>
+          {/* div > 2 button */}
+          <h1 className='banner__description'>{movie?.overview}</h1>
+          {/* Description */}
         </div>
-        {/* div > 2 button */}
-        <h1 className='banner__description'>{movie?.overview}</h1>
-        {/* Description */}
-      </div>
-      <div className='banner--fadeBottom'></div>
-    </header>
-  );
+        <div className='banner--fadeBottom'></div>
+      </header>
+    );
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            src={`http://www.youtube.com/embed/${movie.videos.results[0]?.key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0]?.key}`}
+            width='640'
+            height='360'
+            allow='autoplay; fullscreen'
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
+  }
 };
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.95;
+  border: none;
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
 export default Banner;
