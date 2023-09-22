61e41a3d65da5eddaf41fd4b1276452a98552a7b
rokaf340 <rokaf340@gmail.com>
2023-09-22 23:54:50 +0900

    study

--- src/Pages/SearchPage/index.js
+++ src/Pages/SearchPage/index.js
@@ -29,6 +29,33 @@ function SearchPage() {
       console.log(error);
     }
   };
+
+  const renderSearchResults = () => {
+    return searchResult.length > 0 ? (
+      <section className='search-container'>
+        {searchResult.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== 'person') {
+            const movieImageUrl =
+              'https://image/tmdb.org/t/p/w500' + movie.backdrop_path;
+            return (
+              <div className='movie'>
+                <div className='movie__colimn-poster'>
+                  <img src='{movieImageUrl}' alt='' className='movie__poster' />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className='no-results'>
+        <div className='no-results__text'>
+          <p> 당신이 찾고자 하는 영화 "{searchTerm}"가 없습니다</p>
+        </div>
+      </section>
+    );
+  };
+
   return <div></div>;
 }
 
